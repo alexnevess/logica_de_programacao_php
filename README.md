ea3b30a03b275a5bdd5a078996a0f10dcfa1529c
Alexsandro Neves da Silva Filho <alexsndsf@gmail.com>
2025-05-29 19:04:37 -0300

    Cria o arquivo e att README
    Cria o arquivo verifica_primos_de_1_a_100.php e atualiza o README.md.

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # logica_de_programacao_php
-Repositório com exercícios e desafios de lógica de programação resolvidos em PHP.
+Repositório com exercícios e desafios de **lógica de programação** resolvidos em **PHP**.
---
+++ verifica_primos_de_1_a_100/verifica_primos_de_1_a_100.php
@@ -0,0 +1,31 @@
+//Verifica os números primos de 2 a 100.
+
+<?php
+
+function testa_numero($numero)
+{
+    for($j = 2; $j<$numero; $j++)
+    {
+        $resultado = $numero % $j;
+
+        if($resultado == 0)
+        {
+            return false;
+        }
+    }
+    return true;
+}
+
+$max = 100;
+
+for($i = 2; $i<=$max; $i++)
+{
+  $valor = testa_numero($i);
+
+  if($valor == true)
+  {
+    echo "$i é primo\n";
+  }
+}
+
+?>
