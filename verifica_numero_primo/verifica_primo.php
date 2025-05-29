74243f01e1b98fa3dc59979c2996264e1a89be29
Alexsandro Neves da Silva Filho <alexsndsf@gmail.com>
2025-05-28 21:28:27 -0300

    criação do código verifica_primo.php
    Código de lógica de programação que verifica se um número é primo ou não.

---
+++ verifica_numero_primo/verifica_primo.php
@@ -0,0 +1,26 @@
+//Verifica se o número é primo ou não.
+//Números primos são divisíveis apenas por 1 e por eles mesmos.
+//ou seja, eles não podem ser divisível por qualquer número abaixo dele, que seja diferente de um.
+
+<?php
+
+$n = 4;
+
+
+for ($i = 2; $i < $n; $i++) {
+    $resultado = $n / $i;
+    $restoDivisao = $n % $i;
+
+    if ($restoDivisao == 0)
+    {
+        $divisor = $i;
+        break;
+    }
+}
+
+if (!isset($divisor))
+    echo "O número $n é primo";
+else
+    echo "O número $n não é primo";
+
+?>
